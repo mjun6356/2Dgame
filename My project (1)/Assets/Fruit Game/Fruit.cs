02db090e82489526f830a5e87e22bc93d80280a4
mjun6356 <mjun6356@g.shingu.ac.kr>
2026-04-30 13:05:01 +0900

    [Feat] 수박게임 완성

---
+++ My project (1)/Assets/Fruit Game/Fruit.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+
+    public int fruitType;
+    public bool hasMerged = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasMerged)
+            return;
+
+        Fruit otherFruit = collision.gameObject.GetComponent<Fruit>();
+
+        if (otherFruit != null && !otherFruit.hasMerged && otherFruit.fruitType == fruitType)
+        {
+            hasMerged = true;
+            otherFruit.hasMerged = true;
+
+            Vector3 mergePosition = (transform.position + otherFruit.transform.position) / 2f;
+
+            FruitGame gameManager = FindAnyObjectByType<FruitGame>();
+
+            if (gameManager != null)
+            {
+                gameManager.MergeFruits(fruitType, mergePosition);
+            }
+
+            Destroy(otherFruit.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
